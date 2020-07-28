5bd53b1f5519732cd3b730ec9a347b24af439d78
Malcolm Sparks <mal@juxt.pro>
2020-07-28 23:17:25 +0100

    Promote tested functions to source-code

---
+++ src/crux/pull/alpha/eql/graphql.clj
@@ -0,0 +1,33 @@
+;; Copyright © 2020, JUXT LTD.
+
+(ns crux.pull.alpha.eql.graphql
+  (:require
+   [juxt.reap.alpha.graphql :as reap-graphql]
+   [juxt.reap.alpha.api :as reap]
+   [edn-query-language.core :as eql]
+   [crux.pull.alpha.eql.exec :as exec]
+   [clojure.string :as str]))
+
+(defn eql-keyword [s]
+    (when s
+      (let [[ns n z] (str/split s #"__")]
+        (when z (throw (ex-info "Can only have one double-underscore in a name" {})))
+        (if n
+          (keyword
+           (str/replace ns "_" ".")
+           (str/replace n "_" "."))
+          (keyword (str/replace ns "_" "."))))))
+
+(defmulti selection-to-eql-term first)
+
+(defmethod selection-to-eql-term :field [[_ m]] (eql-keyword (:name m)))
+
+(defn query [query-doc resolver opts]
+  (let [op
+        (first
+         (reap/decode reap-graphql/Document query-doc))
+        selection-set (:selection-set op)
+        eql (mapv selection-to-eql-term selection-set)
+        ast (eql/query->ast eql)]
+    (assert (= (:operation-type op) "query"))
+    (into {} (exec/exec resolver nil ast opts))))
