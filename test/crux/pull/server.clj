9565ed838168c4d230f9c20b706dfe3db4be39ee
Malcolm Sparks <mal@juxt.pro>
2020-07-28 15:49:18 +0100

    Fake response to POST requests on /graphql

--- test/crux/pull/server.clj
+++ test/crux/pull/server.clj
@@ -4,16 +4,52 @@
   (:require
    [integrant.core :as ig]
    [ring.adapter.jetty :as jetty]
+   [jsonista.core :as json]
    [crux.api :as crux]
    [clojure.java.io :as io]))
 
 (defmethod ig/init-key ::server [_ {:keys [crux] :as config}]
   (jetty/run-jetty
    (fn [req]
-     (case (:uri req)
-       "/" {:status 200
-            :headers {"content-type" "text/html;charset=utf-8"}
-            :body (slurp (io/resource "index.html"))}
+     (case [(:request-method req) (:uri req)]
+       [:get "/"]
+       {:status 200
+        :headers {"content-type" "text/html;charset=utf-8"}
+        :body (slurp (io/resource "index.html"))}
+
+       [:post "/graphql"]
+       (let [{operation "operationName"
+              query "query"}
+             (json/read-value (slurp (:body req)))]
+         (case operation
+           "IntrospectionQuery"
+           {:status 200
+            :headers {"content-type" "application/json"}
+            :body (json/write-value-as-string
+                   {"data"
+                    {"__schema"
+                     {"queryType"
+                      {"name" "Root"}
+                      "mutationType" nil
+                      "subscriptionType" nil
+                      "types"
+                      [{"kind" "OBJECT"
+                        "name" "Root"
+                        "description" nil
+                        "fields" []
+                        "inputFields" nil
+                        "interfaces" []
+                        "enumValues" nil
+                        "possibleTypes" nil}]
+                      "directives"
+                      []}}})}
+
+           {:status 200
+            :headers {"content-type" "application/json"}
+            :body (json/write-value-as-string
+                   {"data"
+                    {}})}))
+
        {:status 404
         :body "Not Found"}))
    (conj config [:join? false])))
