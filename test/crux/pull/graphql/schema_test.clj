811e8eaa18b40a1927416d9501f71f8c7b74fc13
Malcolm Sparks <mal@juxt.pro>
2020-08-07 12:16:02 +0100

    checkpoint

--- test/crux/pull/graphql/schema_test.clj
+++ test/crux/pull/graphql/schema_test.clj
@@ -177,148 +177,148 @@
           ;;   a. Return Subscribe(operation, schema, coercedVariableValues, initialValue).
           )))
 
-    (let [attribute-to-graphql-field
-          (fn [attr]
-            (let [{:crux.graphql/keys [name]
-                   :crux.schema/keys [description]
-                   :as args} attr]
-              (prn attr description)
-              ;; Name is mandatory, ex-info is missing!
-              (cond-> {}
-                true (conj
-                      (if name
-                        ["name" name]
-                        (throw (ex-info "Missing :crux.graphql/name" {:attribute attr}))))
-                description (conj ["description" description])
-                ;; TODO: args
-                ;; TODO: type
-                true (conj {"isDeprecated" false})
-                )))
-          entity-to-graphql-type
-          (fn [e]
-            (cond
-              (= e String)
-              {"kind" "SCALAR" "name" "String"}
-              (= e Long)
-              {"kind" "SCALAR" "name" "Long"}
-              (map? e)
-              (let [{:crux.graphql/keys [name]
-                     :crux.schema/keys [description attributes]
-                     t :crux.schema/type}
-                    e]
-                (cond-> {}
-                  t (conj ["kind" "OBJECT"])
-                  name (conj ["name" name])
-                  description (conj ["description" description])
-                  attributes (conj ["fields" (mapv attribute-to-graphql-field (vals attributes))])
-                  ))
-              :else (throw (ex-info "Condition not supported" {:entity e})))
-            )]
-      (with-open
-        [node (crux/start-node {:crux.node/topology '[crux.standalone/topology]})]
-        (crux/await-tx
-         node
-         (crux/submit-tx
-          node
-          (for [ent
-                [{
-                  ;; The Crux id also acts as the name of the relation schema
-                  ;; represented by this entity.
-                  :crux.db/id :ex.type/film
-                  :crux.schema/type :crux.schema.type/relation
-                  :crux.schema/description "A James Bond Film"
-                  :crux.graphql/name "Film"
-                  :crux.schema/attributes
-                  {:film/name
-                   {:crux.graphql/name "filmName"
-                    :crux.schema/required? true}
-                   :film/box
-                   {:crux.schema/type Long
-                    :crux.schema/description "How much the film made at the box office"
-                    :crux.schema/required? true
-                    :crux.graphql/name "box"}
-                   :film/cost
-                   {:crux.schema/description "How much the film cost to make"
-                    :crux.schema/type Long
-                    :crux.schema/required? true
-                    :crux.graphql/name "cost"}
-                   :film/earnings
-                   {:crux.schema/type String
-                    :crux.schema/derived
-                    ^:crux.memoize '(fn [{:film/keys [box cost]}] (- box cost))
-                    :crux.graphql/name "earnings"}
-                   :film/vehicle
-                   {:crux.schema/type :ex.type/vehicle
-                    :crux.schema/cardinality :crux.schema.cardinality/zero-or-more
-                    :crux.graphql/name "vehicles"}}}
+    (defn- attribute-to-graphql-field [attr]
+      (let [{:crux.graphql/keys [name]
+             :crux.schema/keys [description]
+             :as args} attr]
+        (prn attr description)
+        ;; Name is mandatory, ex-info is missing!
+        (cond-> {}
+          true (conj
+                (if name
+                  ["name" name]
+                  (throw (ex-info "Missing :crux.graphql/name" {:attribute attr}))))
+          description (conj ["description" description])
+          ;; TODO: args
+          ;; TODO: type
+          true (conj {"isDeprecated" false}))))
 
-                 {:crux.db/id :ex.type/vehicle
-                  :crux.schema/type :crux.schema.type/relation
-                  :crux.graphql/name "Vehicle"
-                  :crux.schema/attributes
-                  {:vehicle/brand
-                   {:crux.schema/type String
-                    :crux.graphql/name "brand"}
-                   :vehicle/model
-                   {:crux.schema/type String
-                    :crux.graphql/name "model"}}}
+    (defn- entity-to-graphql-type [e]
+      (cond
+        (= e String)
+        {"kind" "SCALAR" "name" "String"}
+        (= e Long)
+        {"kind" "SCALAR" "name" "Long"}
+        (map? e)
+        (let [{:crux.graphql/keys [name]
+               :crux.schema/keys [description attributes]
+               t :crux.schema/type}
+              e]
+          (cond-> {}
+            t (conj ["kind" "OBJECT"])
+            name (conj ["name" name])
+            description (conj ["description" description])
+            attributes (conj ["fields" (mapv attribute-to-graphql-field (vals attributes))])
+            ))
+        :else (throw (ex-info "Condition not supported" {:entity e}))))
 
-                 {:crux.db/id :ex.type/graphql-query-root
-                  :crux.schema/type :crux.schema.type/relation
-                  :crux.graphql/name "Root"
-                  :crux.schema/attributes
-                  {:all-films
-                   {:crux.schema/description "All the films in the James Bond universe."
-                    :crux.schema/type :ex.type/film
-                    :crux.schema/cardinality :crux.schema.cardinality/zero-or-more
-                    :crux.graphql/name "allFilms"
-                    }
-                   :film
-                   {:crux.schema/description "A particular film in the James Bond universe."
-                    :crux.schema/type :ex.type/film
-                    :crux.graphql/name "film"}
-                   }}
+    (with-open
+      [node (crux/start-node {:crux.node/topology '[crux.standalone/topology]})]
+      (crux/await-tx
+       node
+       (crux/submit-tx
+        node
+        (for [ent
+              [{
+                ;; The Crux id also acts as the name of the relation schema
+                ;; represented by this entity.
+                :crux.db/id :ex.type/film
+                :crux.schema/type :crux.schema.type/relation
+                :crux.schema/description "A James Bond Film"
+                :crux.graphql/name "Film"
+                :crux.schema/attributes
+                {:film/name
+                 {:crux.graphql/name "filmName"
+                  :crux.schema/required? true}
+                 :film/box
+                 {:crux.schema/type Long
+                  :crux.schema/description "How much the film made at the box office"
+                  :crux.schema/required? true
+                  :crux.graphql/name "box"}
+                 :film/cost
+                 {:crux.schema/description "How much the film cost to make"
+                  :crux.schema/type Long
+                  :crux.schema/required? true
+                  :crux.graphql/name "cost"}
+                 :film/earnings
+                 {:crux.schema/type String
+                  :crux.schema/derived
+                  ^:crux.memoize '(fn [{:film/keys [box cost]}] (- box cost))
+                  :crux.graphql/name "earnings"}
+                 :film/vehicle
+                 {:crux.schema/type :ex.type/vehicle
+                  :crux.schema/cardinality :crux.schema.cardinality/zero-or-more
+                  :crux.graphql/name "vehicles"}}}
 
-                 ]]
-            [:crux.tx/put ent])))
+               {:crux.db/id :ex.type/vehicle
+                :crux.schema/type :crux.schema.type/relation
+                :crux.graphql/name "Vehicle"
+                :crux.schema/attributes
+                {:vehicle/brand
+                 {:crux.schema/type String
+                  :crux.graphql/name "brand"}
+                 :vehicle/model
+                 {:crux.schema/type String
+                  :crux.graphql/name "model"}}}
 
-        (let [db (crux/db node)
-              schema (-> {"queryType" "Root"
-                          "types"
-                          (mapv entity-to-graphql-type
-                                [(crux/entity db :ex.type/graphql-query-root)
-                                 ;; TODO: All the rest should be discovered via graph traversal
-                                 (crux/entity db :ex.type/film)
-                                 (crux/entity db :ex.type/vehicle)
-                                 String
-                                 ])
-                          "directives" []}
-                         validate-schema)]
+               {:crux.db/id :ex.type/graphql-query-root
+                :crux.schema/type :crux.schema.type/relation
+                :crux.graphql/name "Root"
+                :crux.schema/attributes
+                {:all-films
+                 {:crux.schema/description "All the films in the James Bond universe."
+                  :crux.schema/type :ex.type/film
+                  :crux.schema/cardinality :crux.schema.cardinality/zero-or-more
+                  :crux.graphql/name "allFilms"
+                  }
+                 :film
+                 {:crux.schema/description "A particular film in the James Bond universe."
+                  :crux.schema/type :ex.type/film
+                  :crux.graphql/name "film"}
+                 }}
 
-          ;; Extract GraphQL types to file, just to compare
-          (spit "/tmp/schema.json"
-                (json/generate-string
-                 {:data
-                  {"__schema" schema}}
-                 {:pretty true}))
+               ]]
+          [:crux.tx/put ent])))
 
-          ;; Query the schema with GraphQL execution
+      (let [db (crux/db node)
+            schema
+            (-> {"queryType" "Root"
+                 "types"
+                 (mapv entity-to-graphql-type
+                       [(crux/entity db :ex.type/graphql-query-root)
+                        ;; TODO: All the rest should be discovered via graph traversal
+                        (crux/entity db :ex.type/film)
+                        (crux/entity db :ex.type/vehicle)
+                        String
+                        ])
+                 "directives" []}
+                validate-schema)]
 
-          (let [document (->
-                          "query GetFilms { allFilms { filmName }}"
-                          #_introspection-query
-                          graphql/parse-graphql
-                          graphql/validate-graphql-document)]
+        ;; Extract GraphQL types to file, just to compare
+        (spit "/tmp/schema.json"
+              (json/generate-string
+               {:data
+                {"__schema" schema}}
+               {:pretty true}))
 
-            ;; TODO: Introspection (Attempt to execute this query against the schema)
+        ;; Query the schema with GraphQL execution
 
-            ;;
-            (execute-request
+        (let [document (->
+                        "query GetFilms { allFilms { filmName }}"
+                        #_introspection-query
+                        graphql/parse-graphql
+                        graphql/validate-graphql-document)]
+
+          ;; TODO: Introspection (Attempt to execute this query against the schema)
+
+          schema
+          ;;
+          #_(execute-request
              {:schema schema
               :document document
               :operation-name "GetFilms" #_"IntrospectionQuery"
               :variable-values {}
-              :initial-value (crux/entity db :ex.type/graphql-query-root)})))))))
+              :initial-value (crux/entity db :ex.type/graphql-query-root)}))))))
 
 ;; https://en.wikipedia.org/wiki/Functional_dependency
 ;; https://en.wikipedia.org/wiki/Armstrong%27s_axioms
